bf9d1ea4d91c8c39ac4a8f1c2925aa84d578d38e
czuniga42 <czuniga-@student.42madrid.com>
2025-06-12 22:20:43 +0200

    jueves 2

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: czuniga- <czuniga-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 22:57:31 by czuniga-          #+#    #+#             */
-/*   Updated: 2025/06/12 18:21:34 by czuniga-         ###   ########.fr       */
+/*   Updated: 2025/06/12 19:33:36 by czuniga-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,33 @@ typedef struct	s_data
 		unsigned char	c;
 		char			*message;
 		size_t			message_len;
-	} t_data;
+	}	t_data;
 
-	t_data	*g_data = NULL;
+t_data	*g_data;
+
+void	ft_free_and_print(void)
+{
+	ft_putstr_fd(g_data->message, 1);
+	free(g_data->message);
+	g_data->message = NULL;
+	g_data->message_len = 0;
+	g_data->client_pid = 0;
+}
+
+void	ft_init_data(t_data *data)
+{
+	data->client_pid = 0;
+	data->c = 0;
+	data->count_bit = 0;
+	data->message = NULL;
+	data->message_len = 0;
+}
 
 void	ft_reserve_space(void)
 {
-	char *new_message
+	char *new_message;
 	
-	new_message = malloc (g_data->message_len + 2);
+	new_message = malloc(g_data->message_len + 2);
 	if (!new_message)
 		exit (1);
 	if (g_data->message)
@@ -47,54 +65,42 @@ void	ft_signal_handler(int signal, siginfo_t *info, void *context)
 {
 	(void)context;
 	if(g_data->client_pid == 0)
-		g_data->client_pid = info->si->pid;
+		g_data->client_pid = info->si_pid;
 	if (info->si_pid != g_data->client_pid)
 		return ;
 	if (signal == SIGUSR1)
 		g_data->c |= (1 << g_data->count_bit);
 	g_data->count_bit++;
 	if (g_data->count_bit == 8)
-	
-	
-	write(1, "Bit recibido: ", 14);
-	if (signal == SIGUSR1)
-		write(1, "1\n", 2);
-	else
-		write(1, "0\n", 2);
-
-	write(1, "Estado de c: ", 13);
-	print_bits(c);
-	count_bit++;
-	if (count_bit == 8) //cuando ya tiene los 8 bits)
 	{
-		write(1, &c, 1);
-		write(1, "\n", 1);
-		count_bit = 0;
-		c = 0;
+		ft_reserve_space(); // ???
+		if (g_data->c == '\0')
+			ft_free_and_print();
+		g_data->count_bit = 0;
+		g_data->c = 0;
 	}
 	kill(info->si_pid, SIGUSR1);
 }
-
+	
 int main(void)
 {
-	struct	sigaction	sa;
+	struct	sigaction sa;
 	g_data = malloc(sizeof(t_data));
 	if (!g_data)
 		return (1);
-	init_data(g_data);
+	ft_init_data(g_data);
 	sa.sa_sigaction = ft_signal_handler;
 	sa.sa_flags = SA_SIGINFO;
 	sigemptyset(&sa.sa_mask);
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-	ft_putstr_ft("servidor PID: ", 1);
+	ft_putstr_fd("servidor PID: ", 1);
 	ft_putnbr_fd(getpid(), 1);
 	ft_putchar_fd('\n', 1);
 	while (1)
 		pause();
 	return (0);
 }
-
 
 /* signal es para SIGUSR1 Y 2, sigaction, etc..
 1 = 1 bit, 2 = 0 bits
