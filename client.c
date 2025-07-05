73b06d5d8d7109bfe84cf676a0bb550fc1728556
Carolina Zuniga Alache <czuniga-@c2r18s4.42madrid.com>
2025-07-05 13:50:42 +0200

    coments 1

--- client.c
+++ client.c
@@ -6,18 +6,18 @@
 /*   By: czuniga- <czuniga-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 22:55:03 by czuniga-          #+#    #+#             */
-/*   Updated: 2025/07/05 13:03:36 by czuniga-         ###   ########.fr       */
+/*   Updated: 2025/07/05 13:45:35 by czuniga-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
 
-volatile sig_atomic_t	g_ack = 0;
-
+volatile sig_atomic_t	g_ack = 0; // g_ack: variable global usada como flag para saber cuando se ha recibido una señal de confirmacion (ACK) del servidor
+//volatile sig_atomic_t  asegura el  acces desd una señal handler
 void	ft_ack_handler(int signum)
 {
 	(void)signum;
-	g_ack = 1;
+	g_ack = 1; // solo iguala a 1 para indicar que se recibió un ACK
 }
 
 void	ft_send_bit(pid_t pid, int bit)
@@ -33,7 +33,7 @@ void	ft_send_bit(pid_t pid, int bit)
 		kill(pid, SIGUSR2);
 		write(1, "1", 1);
 	}
-	while (!g_ack)
+	while (!g_ack) //espera la confirmacion del servidor hasta que g_ack cambie a 1
 		pause();
 }
 
@@ -59,20 +59,20 @@ int	main(int argc, char **argv)
 
 	if (argc != 3)
 		return (ft_putstr_fd("Usage: ./client <PID> <message>\n", 2), 1);
-	pid = (pid_t)(ft_atoi(argv[1]));
-	msg = argv[2];
-	len = ft_strlen(msg);
-	signal(SIGUSR1, ft_ack_handler);
+	pid = (pid_t)(ft_atoi(argv[1])); //convierte el pid que esta como argumento a entero
+	msg = argv[2]; //guarda el mensaje
+	len = ft_strlen(msg); //guarda la longitud de mensaje
+	signal(SIGUSR1, ft_ack_handler);//toma dos argumentos: el entero que representa la señal(SIGUSR1 o SIGUSR2), y un puntero a una funcion.-signal: forma sencilla de manejar señalesdel sistema operativo como i
 	i = 31;
 	while (i >= 0)
-		ft_send_bit(pid, (len >> i--) & 1);
+		ft_send_bit(pid, (len >> i--) & 1); //1ro esperara por 32 bits que es el tamaño completo del mensaje
 	write(1, " ", 1);
-	ft_send_char(pid, 0);
+	ft_send_char(pid, 0); //envia 8bits (0)para indica que ya envio el tamaño y toca el  mensaje. seusa como separador
 	i = 0;
 	while (i < len)
-		ft_send_char(pid, msg[i++]);
-	ft_send_char(pid, 0);
-	usleep(800);
+		ft_send_char(pid, msg[i++]); //aqui envia el mensaje
+	ft_send_char(pid, 0); //aqui vuelve a separar con ceros
+	usleep(800); //espera 800 microsegundos por si hay retrasos
 	ft_putstr_fd("\nMensaje enviado correctamente.\n", 1);
 	return (0);
 }
